a31aa0c741f257a031072270cb2c52886dcd631f
Zack Borges <zackartborges@gmail.com>
2023-04-28 19:31:50 -0400

    adds api call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('/outfit')
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {!data ? 'Loading...' : data}
         </p>
         <a
           className="App-link"
